d290e9e36f26ec963baa6d37833b7ce7e5b717db
Zaif-K <159206675+Zaif-K@users.noreply.github.com>
2024-08-14 12:17:39 +1000

    worked on arduino
    updated and learnt basics of code

--- 01.serialMonitor/01.serialMonitor.ino
+++ 01.serialMonitor/01.serialMonitor.ino
@@ -1,5 +1,5 @@
 /*
-  Author: 
+  Author: Zaif Khan 13/8/2024
   
   Learning Intention:
   The students will understand the difference between the inbuilt setup and loop methods,
@@ -13,7 +13,10 @@
     5.  I understand `//` and `\/* *\/` comments 
   
   Student Notes: 
-
+  Only one void setup and setup in the code
+  Make sure the baud is the same across the code otherwise it will break
+  Use serial.monitor to debug code- serial.monitor
+  Loops go on infinitely but set up goes for only once when inputted
   Documentation: 
     https://www.arduino.cc/reference/en/language/functions/communication/serial/
     https://docs.arduino.cc/software/ide-v2/tutorials/ide-v2-serial-plotter
@@ -21,11 +24,21 @@
 
 // The setup function runs once when you press reset or power the board
 void setup() {
-
+ Serial.begin(9600);
+ Serial.print("Serial Monitor debug");
 }
 
 // The loop function runs over and over again forever
 void loop() {
+  Serial.print("Pin-A0:");
+  Serial.print(analogRead(A0));
+  Serial.print(",");
+  Serial.print("Pin-A1:");
+  Serial.print(analogRead(A1));
+  Serial.print(",");
+  Serial.print("Pin-A2:");
+  Serial.println(analogRead(A2));
+
 
 }
 
